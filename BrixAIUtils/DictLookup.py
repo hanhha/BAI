ba71e06c2a642ff4fecd769699c3e8f3a10b2bf4
Hanh Ha <haminhtranhanh@gmail.com>
2017-11-03 22:23:29 +0700

    Revised DictLookup to avoid error incase of no prunciation

--- BrixAIUtils/DictLookup.py
+++ BrixAIUtils/DictLookup.py
@@ -10,7 +10,9 @@ def lookup (word):
 
     if len(raw_of_pron) is not 0:
         pron = raw_of_pron[0].text_content()
+    if len(raw_of_desc) is not 0:
         desc = raw_of_desc[0].text_content()
+    if len(raw_of_rela) is not 0:
         rela = [item.text_content() for item in raw_of_rela]
 
     return pron, desc, rela
